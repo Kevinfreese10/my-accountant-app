dab961c691d95c2f8c872a2cd6665d9f3dcaf9b7
Kevin Freese <kev@thinkestry.co.za>
2025-12-16 05:10:09 +0000

    Is robots.txt also updated?

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,7 @@
+
 import { MetadataRoute } from 'next';
 
-const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || 'https://www.myacc.co.za';
+const BASE_URL = process.env.NEXT_PUBLIC_APP_URL;
 
 export default function robots(): MetadataRoute.Robots {
   return {
