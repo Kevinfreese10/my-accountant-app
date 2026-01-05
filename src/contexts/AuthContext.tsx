fe61ef4a0c0a45735eb304e25eb4a5459fdc19c6
Kevin Freese <kev@thinkestry.co.za>
2026-01-05 12:57:11 +0000

    Try fixing this error: `Console Error: Error: serverTimestamp is not def

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,7 +3,7 @@
 import { createContext, useState, useContext, ReactNode, useEffect } from 'react';
 import type { User } from '@/lib/types';
 import { useRouter } from 'next/navigation';
-import { getFirestore, collection, query, where, getDocs, doc, setDoc, updateDoc, getDoc } from 'firebase/firestore';
+import { getFirestore, collection, query, where, getDocs, doc, setDoc, updateDoc, getDoc, serverTimestamp } from 'firebase/firestore';
 import { firebaseApp } from '@/lib/firebase';
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, User as FirebaseUser, signOut, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
 import { errorEmitter } from '@/firebase/error-emitter';
