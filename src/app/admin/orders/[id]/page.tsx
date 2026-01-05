3be5e840c419b5bbcddf62f4b0e4e9ead5e7ecdd
Kevin Freese <kev@thinkestry.co.za>
2026-01-05 16:41:47 +0000

    Show the clickable link in blue so that is easy to spot

--- src/app/admin/orders/[id]/page.tsx
+++ src/app/admin/orders/[id]/page.tsx
@@ -373,7 +373,7 @@ export default function AdminOrderDetailsPage() {
       } else if (type === 'docs') {
           text += `This is a reminder to please upload the required documents for your order #${orderId} so that we can begin processing it.\n\n`;
       } else if (type === 'review') {
-          text += `We hope you were happy with our service for order #${orderId}. If you have a moment, we would greatly appreciate it if you could leave us a review on Google at the link below:\n\n<a href="https://g.page/r/CVIOzn2bYoiaEAE/review" target="_blank">https://g.page/r/CVIOzn2bYoiaEAE/review</a>\n\n`;
+          text += `We hope you were happy with our service for order #${orderId}. If you have a moment, we would greatly appreciate it if you could leave us a review on Google at the link below:\n\n<a href="https://g.page/r/CVIOzn2bYoiaEAE/review" target="_blank" style="color: blue;">https://g.page/r/CVIOzn2bYoiaEAE/review</a>\n\n`;
       } else if (type === 'discount') {
           text += `As a token of our appreciation for your business, here is a 10% discount code for your next order: WELCOME10\n\n`;
       }
