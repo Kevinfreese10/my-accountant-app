84b49227e885b804018f51fec07d53456e4fa4df
Kevin Freese <kev@thinkestry.co.za>
2026-04-23 13:06:19 +0000

    Try fixing this error: `Runtime Error: Error: FormDescription is not def

--- src/components/admin/ServiceForm.tsx
+++ src/components/admin/ServiceForm.tsx
@@ -1,10 +1,11 @@
+
 'use client';
 import { useForm, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Service } from '@/lib/types';
 import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
